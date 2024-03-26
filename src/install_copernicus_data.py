e0863b3302cfa4576a529e1e2d83aa6b9b1aa199
George Husband <ghus266@gmail.com>
2024-03-26 17:22:41 +0100

    in install script added: getting pass & email, token, selecting max feautres per year

--- src/install_copernicus_data.py
+++ src/install_copernicus_data.py
@@ -83,31 +83,79 @@ def select_ids(df: pd.DataFrame, number: int) -> pd.core.frame.DataFrame:
     return df.loc[:, ["Id", "Name", "Footprint"]][:number]
 
 
+def get_access_token(email: str, password: str) -> str:
+    """Returns a access token that will be used as a key to install data
+    Inputs:
+        email: str -> email used to connect to sentinel hub
+        password: str -> password used to connect to sentinel hub
+    Outputs:
+        access_token: str -> access token that will be used as key to download data
+    """
+    # Define the endpoint and payload
+    url = "https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token"
+    payload = {
+        "username": email.strip(),
+        "password": password.strip(),
+        "grant_type": "password",
+        "client_id": "cdse-public",
+    }
+
+    # Make the HTTP POST request
+    response = requests.post(
+        url, data=payload, headers={"Content-Type": "application/x-www-form-urlencoded"}
+    )
+
+    data = response.json()
+    access_token = data.get("access_token")
+    return access_token
+
+
+def get_credentials_from_file(path: str) -> (str, str):
+    """Extracts email and password from a text file containing 2 lines, on the first line email, on the second line password. This allows the user to not have to type it in every time
+    Inputs:
+        path: str -> path to text file containing email and password on seperate lines
+    Outputs:
+        (email, password): (str, str) -> tuple containing email and password
+    """
+    with open(path, "r") as f:
+        email: str = f.readline()
+        password: str = f.readline()
+        return (email, password)
+
+
 def main():
     json_path = input("Input location to json mask: ")
     default_json_path = os.path.join(
         "..", "sections_mfe", "buffers", "A43_33_44_short.geojson"
     )
-    if json_path == "":
+    if json_path == "":  # if no user input then use use default path to mask layer
         json_path = default_json_path
 
-    gdf = import_mask_layer(json_path)
-    years_to_get = [2015, 2016, 2017]  # get query year by year
+    credential_file_path = input("Input location of credential file: ")
+    default_credential_file_path = os.path.join("..", "mydata", "pass.txt")
+    if credential_file_path == "":
+        credential_file_path = default_credential_file_path
 
-    data_per_year = [make_request(x, 20, get_wkt(gdf)) for x in years_to_get]
-    print(type(select_ids(data_per_year[0], 2)))
+    email, password = get_credentials_from_file(credential_file_path)
+    print("-> Getting Access token ", end="")
+    access_token = get_access_token(email, password)
+    print("✔")
+
+    print("-> Opening mask layer ", end="")
+    gdf = import_mask_layer(json_path)
+    print("✔")
+
+    years_to_get = [2015, 2016, 2017]  # get query year by year
+    data_per_year: list = [make_request(x, 20, get_wkt(gdf)) for x in years_to_get]
+    # TODO: filter dataframes by geometry to ensure a complete overlap
+    data_per_year_selected: list = [select_ids(x, 3) for x in data_per_year]
+
+    # downloading data
+    for dataframe in data_per_year_selected:
+        for row in dataframe:
+            # TODO: install data
+            pass
 
 
 if __name__ == "__main__":
     main()
-
-
-"""
-https://catalogue.dataspace.copernicus.eu/odata/v1/Products?$filter=OData.CSC.Intersects(area=geography'SRID=4326;POLYGON ((5.256018566739281 45.60328671462783, 5.25608161980
-865 45.60358096228486, 5.257323280251573 45.60332632488937, 5.25854230625934 45.60308057959336, 5.258928708402387 45.60303207723231, 5.259389480832378 45.60293992274634, 5.25
-9675644762583 45.60288818689452, 5.261717506499203 45.60244673283748, 5.264005482538052 45.60198721538676, 5.264265584863447 45.60192945689108, 5.264216529473862 45.601621849
-2632, 5.264101154727738 45.6016386796606, 5.261813690039623 45.6020980993951, 5.259525701558204 45.60255849115758, 5.259221339314066 45.6026440583439, 5.258670029143444 45.60
-275238028357, 5.258419433611341 45.60280573288075, 5.257202024348945 45.60304662794061, 5.256018566739281 45.60328671462783))') and Collection/Name eq 'SENTINEL-2' and Conten
-tDate/Start gt 2015-01-01T00:00:00.000Z and ContentDate/Start lt 2017-12-01T00:00:00.000Z and Attributes/OData.CSC.DoubleAttribute/any(att:att/Name eq 'cloudCover' and att/OD
-ata.CSC.DoubleAttribute/Value lt 20)
-"""
